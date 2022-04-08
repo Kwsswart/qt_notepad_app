69d7fef85fb92a4b2db26b2625a3675b7d6542f8
Kieron Spearing <kieron@stylesage.co>
2022-04-08 18:03:19 +0200

    Add buttons and functionality to each

--- qt_notepad/mainwindow.cpp
+++ qt_notepad/mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit); // This will be the central widget to take up the entire screen.
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +14,92 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_actionNew_triggered()
+{
+    currentFile.clear();
+    ui->textEdit->setText(QString());
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Open the file");
+    QFile file(fileName);
+    currentFile = fileName;
+
+    if(!file.open(QIODevice::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot open file: " + file.errorString());
+        return;
+    }
+
+    setWindowTitle(fileName);
+    QTextStream input(&file);
+    QString text = input.readAll();
+    ui->textEdit->setText(text);
+
+    file.close();
+}
+
+void MainWindow::on_actionSave_as_triggered()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, "Save as");
+    QFile file(fileName);
+
+    if(!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
+        return;
+    }
+
+    currentFile = fileName;
+    setWindowTitle(fileName);
+
+    QTextStream output(&file);
+    QString text = ui->textEdit->toPlainText();
+    output << text;
+
+    file.close();
+}
+
+void MainWindow::on_actionPrint_triggered()
+{
+    QPrinter printer;
+    printer.setPrinterName("Name of Printer");
+    QPrintDialog printDialog(&printer, this);
+
+    if (printDialog.exec() == QDialog::Rejected) {
+        QMessageBox::warning(this, "Warning", "Cannot access printer");
+        return ;
+    }
+
+    ui->textEdit->print(&printer);
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    QApplication::quit();
+}
+
+void MainWindow::on_actionCopy_triggered()
+{
+    ui->textEdit->copy();
+}
+
+void MainWindow::on_actionPaste_triggered()
+{
+    ui->textEdit->paste();
+}
+
+void MainWindow::on_actionCut_triggered()
+{
+    ui->textEdit->cut();
+}
+
+
+void MainWindow::on_actionUndo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+void MainWindow::on_actionRedo_triggered()
+{
+    ui->textEdit->redo();
+}
