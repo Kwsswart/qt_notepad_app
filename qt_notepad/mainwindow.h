5c79ecc60e37454252eee9f3392700ed0eddcd96
Kieron Spearing <kieron@stylesage.co>
2022-04-08 17:59:13 +0200

    Import required modules,create currentFile variable && add icon resources"

--- qt_notepad/mainwindow.h
+++ qt_notepad/mainwindow.h
@@ -3,8 +3,17 @@
 
 #include <QMainWindow>
 
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QPrinter>
+#include <QPrintDialog>
+
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+    class MainWindow;
+}
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -12,10 +21,11 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
+    QString currentFile = "";
 };
 #endif // MAINWINDOW_H
